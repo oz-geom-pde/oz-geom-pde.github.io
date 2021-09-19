d7b2f279576c3e6a7134c62c52e8072d83c1c6c0
Paul Bryan <pabryan@gmail.com>
2021-09-20 01:23:58 +1000

    Dynamically load posts.

--- js/local.js
+++ js/local.js
@@ -1,9 +1,15 @@
+var loaded = [];
+
 $(document).ready(function(){
     $('.collapse').on('show.bs.collapse', function () {
-	$(this).parent().find(".fa").toggleClass('fa-caret-down fa-caret-up');
+	$(this).parent().find(".abstract-arrow").toggleClass('fa-caret-down fa-caret-up');
+	if (! loaded.includes($(this).attr("id"))) {
+	    $(this).load($(this).attr("data-post"), function(){MathJax.typeset($(this));});
+	    loaded.push($(this).attr("id"));
+	}
     });
 
     $('.collapse').on('hide.bs.collapse', function () {
-	$(this).parent().find(".fa").toggleClass('fa-caret-up fa-caret-down');
+	$(this).parent().find(".abstract-arrow").toggleClass('fa-caret-up fa-caret-down');
     });
 });
