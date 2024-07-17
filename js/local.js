779c0064c3787dc3a80a96ed1d371a978e1d0b9c
Paul Bryan <pabryan@gmail.com>
2024-07-17 12:11:59 +1000

    Upcoming talks marked as such.

--- js/local.js
+++ js/local.js
@@ -13,3 +13,17 @@ $(document).ready(function(){
 	$(this).parent().find(".abstract-arrow").toggleClass('fa-caret-up fa-caret-down');
     });
 });
+
+function getCompareDate() {
+  var d = new Date(),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+  if (month.length < 2) month = '0' + month;
+  if (day.length < 2) day = '0' + day;
+  return [year, month, day].join('');
+}
+
+$('[post-date]').each(function() {
+  if($(this).attr('post-date') > getCompareDate()) $(this).show();
+});
